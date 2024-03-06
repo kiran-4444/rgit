3f0796c61a96efab1dbc41b0c57e19ac78cd9340
Chandra Kiran G <chandrakiran.g19@gmail.com>
2024-03-06 19:28:45 +0530

    feat: add tests for init

---
+++ tests/init.rs
@@ -0,0 +1,32 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use std::process::Command; // Run programs
+
+#[test]
+fn test_init_with_no_args() {
+    let mut cmd = Command::cargo_bin("r_git").unwrap();
+    cmd.arg("init").assert().success();
+
+    // check if the .rgit directory is created
+    assert!(std::path::Path::new(".rgit").exists());
+
+    // check if objects directory is created
+    assert!(std::path::Path::new(".rgit/objects").exists());
+
+    // remove the .rgit directory
+    std::fs::remove_dir_all(".rgit").unwrap();
+}
+
+#[test]
+fn test_init_with_args() {
+    let mut cmd = Command::cargo_bin("r_git").unwrap();
+    cmd.arg("init").arg("sub_path").assert().success();
+
+    // check if the .rgit directory is created
+    assert!(std::path::Path::new("sub_path/.rgit").exists());
+
+    // check if objects directory is created
+    assert!(std::path::Path::new("sub_path/.rgit/objects").exists());
+
+    // remove the .rgit directory
+    std::fs::remove_dir_all("sub_path/.rgit").unwrap();
+}
