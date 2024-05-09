684c27d382538f1509e501d73105cdcb016c383e
Chandra Kiran G <chandrakiran.g19@gmail.com>
2024-05-09 18:23:51 +0530

    feat: handle editing staged file and refactor

--- src/command/status.rs
+++ src/command/status.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use colored::Colorize;
 
 use crate::{
-    database::Database,
+    database::{Database, FlatTree},
     index::{FlatIndex, Index},
     utils::{decompress_content, get_root_path, write_to_stdout},
     workspace::WorkspaceTree,
@@ -19,16 +19,17 @@ impl StatusCMD {
         let root_path = get_root_path()?;
         let git_path = root_path.join(".rgit");
         let database = Database::new(git_path.join("objects"));
+
         let flat_commit_tree = database.read_head()?;
 
         let workspace = WorkspaceTree::new(Some(&root_path));
-        let mut index = Index::new(root_path.join(".rgit").join("index"));
-        index.load()?;
         let mut flat_workspace = FlatIndex {
             entries: Default::default(),
         };
         Index::flatten_entries(&workspace.workspace, &mut flat_workspace);
 
+        let mut index = Index::new(root_path.join(".rgit").join("index"));
+        index.load()?;
         let mut flat_index = FlatIndex {
             entries: Default::default(),
         };
@@ -38,54 +39,21 @@ impl StatusCMD {
             .entries
             .iter()
             .filter(|(path, _)| {
-                !flat_workspace.entries.contains_key(*path)
-                    && !flat_index.entries.contains_key(*path)
-                    || flat_workspace.entries.contains_key(*path)
-                        && !flat_index.entries.contains_key(*path)
-                        && flat_commit_tree.entries.contains_key(*path)
+                !flat_index.entries.contains_key(*path)
+                    && flat_commit_tree.entries.contains_key(*path)
             })
             .map(|(path, _)| path)
             .collect::<Vec<_>>();
 
-        let untracked_files = flat_workspace
-            .entries
-            .iter()
-            .filter(|(path, _)| !flat_index.entries.contains_key(*path))
-            .map(|(path, _)| path)
-            .collect::<Vec<_>>();
-
+        let untracked_files = self.untracked_files(&flat_workspace, &flat_index, &flat_commit_tree);
         write_to_stdout("Untracked files:")?;
-        for file in untracked_files {
+        for (file, _) in untracked_files {
             println!("{}", file.red());
         }
 
-        let staged_files = flat_index
-            .entries
-            .iter()
-            .filter_map(|(path, _)| {
-                // if there's no key in the commit tree, then this will be a new file
-                if !flat_commit_tree.entries.contains_key(path) {
-                    return Some((path.clone(), "new file"));
-                }
-                let commit_tree_entry = flat_commit_tree.entries.get(path).unwrap();
-                let index_entry = flat_index.entries.get(path).unwrap();
-                let index_oid = index_entry.oid.as_ref().unwrap();
-                let index_decompressed_content = decompress_content(&index_oid).unwrap();
-                let commit_tree_file_oid = commit_tree_entry.oid.as_ref().unwrap();
-                let commit_tree_file_content = decompress_content(&commit_tree_file_oid).unwrap();
-
-                if index_decompressed_content != commit_tree_file_content {
-                    return Some((path.clone(), "modified"));
-                }
-                None
-            })
-            .collect::<BTreeMap<_, _>>();
+        let staged_files = self.tracked_files(&flat_workspace, &flat_index, &flat_commit_tree);
 
         write_to_stdout("Changes to be committed:")?;
-        for file in tracked_deleted_files.clone() {
-            let prompt = format!("deleted: {}", file);
-            println!("{}", prompt.green());
-        }
         for (file, status) in staged_files.clone() {
             println!("{}: {}", status.green(), file.green());
         }
@@ -94,22 +62,24 @@ impl StatusCMD {
             .entries
             .iter()
             .filter_map(|(path, _)| {
-                let commit_tree_entry = flat_commit_tree.entries.get(path).unwrap();
                 // if there's no key in the workspace, then this will be a deleted file
                 if !flat_workspace.entries.contains_key(path) {
-                    if !tracked_deleted_files.contains(&path) {
+                    if flat_index.entries.contains_key(path) {
                         return Some((path.clone(), "deleted"));
                     }
                     return None;
                 }
                 let workspace_entry = flat_workspace.entries.get(path).unwrap();
-                let workspace_file_content = fs::read_to_string(&workspace_entry.path).unwrap();
-                let commit_entry_oid = commit_tree_entry.oid.as_ref().unwrap();
-                let decompressed_content = decompress_content(&commit_entry_oid).unwrap();
-                if workspace_file_content != decompressed_content
-                    && !staged_files.contains_key(path.as_str())
-                    && !tracked_deleted_files.contains(&path)
-                {
+                let workspace_entry_content = fs::read_to_string(&workspace_entry.path).unwrap();
+
+                if !flat_index.entries.contains_key(path) {
+                    return None;
+                }
+
+                let index_entry = flat_index.entries.get(path).unwrap();
+                let index_entry_oid = index_entry.oid.as_ref().unwrap();
+                let index_entry_content = decompress_content(&index_entry_oid).unwrap();
+                if index_entry_content != workspace_entry_content {
                     return Some((path.clone(), "modified"));
                 }
                 None
@@ -122,5 +92,96 @@ impl StatusCMD {
         }
 
         Ok(())
+    }
+
+    /// Returns a map of tracked files and their status
+    /// If a file is in the index but not in the commit tree, it's a new file
+    /// If a file is in the index and the commit tree but the content is different, it's a modified file
+    /// If a file is in the commit tree but not in the index, it's a deleted file
+    fn tracked_files(
+        &self,
+        workspace: &FlatIndex,
+        index: &FlatIndex,
+        commit_tree: &FlatTree,
+    ) -> BTreeMap<String, String> {
+        let mut tracked_files = BTreeMap::new();
+        for (path, _) in index.entries.iter() {
+            if !commit_tree.entries.contains_key(path) {
+                tracked_files.insert(path.clone(), "new file".to_string());
+                continue;
+            }
+
+            let commit_entry = commit_tree.entries.get(path).unwrap();
+            let commit_entry_oid = commit_entry.oid.as_ref().unwrap();
+            let commit_entry_content = decompress_content(&commit_entry_oid).unwrap();
+
+            let index_entry = index.entries.get(path).unwrap();
+            let index_entry_oid = index_entry.oid.as_ref().unwrap();
+            let index_entry_content = decompress_content(&index_entry_oid).unwrap();
+
+            if index_entry_content != commit_entry_content {
+                tracked_files.insert(path.clone(), "modified".to_string());
+            }
+        }
+
+        for (path, _) in commit_tree.entries.iter() {
+            if !index.entries.contains_key(path) {
+                tracked_files.insert(path.clone(), "deleted".to_string());
+            }
+        }
+        tracked_files
+    }
+
+    /// Returns a map of untracked files
+    /// If a file is in the workspace but not in the index, it's untracked and new
+    /// If a file is in the index but not in the workspace, it's untracked and deleted
+    fn untracked_files(
+        &self,
+        workspace: &FlatIndex,
+        index: &FlatIndex,
+        commit_tree: &FlatTree,
+    ) -> BTreeMap<String, String> {
+        let mut untracked_files = BTreeMap::new();
+        for (path, _) in workspace.entries.iter() {
+            if !index.entries.contains_key(path) {
+                untracked_files.insert(path.clone(), "untracked".to_string());
+            }
+        }
+
+        untracked_files
+    }
+
+    fn modified_files(
+        &self,
+        workspace: &FlatIndex,
+        index: &FlatIndex,
+        commit_tree: &FlatTree,
+    ) -> BTreeMap<String, String> {
+        let mut modified_files = BTreeMap::new();
+
+        for (path, _) in commit_tree.entries.iter() {
+            if !workspace.entries.contains_key(path) {
+                if index.entries.contains_key(path) {
+                    modified_files.insert(path.clone(), "deleted".to_string());
+                }
+                continue;
+            }
+
+            let workspace_entry = workspace.entries.get(path).unwrap();
+            let workspace_entry_content = fs::read_to_string(&workspace_entry.path).unwrap();
+
+            if !index.entries.contains_key(path) {
+                continue;
+            }
+
+            let index_entry = index.entries.get(path).unwrap();
+            let index_entry_oid = index_entry.oid.as_ref().unwrap();
+            let index_entry_content = decompress_content(&index_entry_oid).unwrap();
+            if index_entry_content != workspace_entry_content {
+                modified_files.insert(path.clone(), "modified".to_string());
+            }
+        }
+
+        modified_files
     }
 }
--- tests/command/status.rs
+++ tests/command/status.rs
@@ -445,3 +445,63 @@ Changed not staged for commit:";
 
     Ok(())
 }
+
+#[test]
+fn test_modify_add_modify_status() -> Result<()> {
+    let temp_dir = TempDir::new("test_rgit").expect("Failed to create temp dir");
+    setup_fs(&temp_dir).expect("Failed to setup fs");
+    setup_rgit(&temp_dir.path().to_path_buf()).expect("Failed to setup rgit");
+
+    let mut cmd = get_rgit_cmd();
+    cmd.current_dir(&temp_dir)
+        .arg("add")
+        .arg("a.txt")
+        .assert()
+        .success();
+
+    let mut cmd = get_rgit_cmd();
+    cmd.current_dir(&temp_dir)
+        .arg("commit")
+        .arg("-m")
+        .arg("Initial commit")
+        .assert()
+        .success();
+
+    write(temp_dir.path().join("a.txt"), "modified").expect("Failed to write to file");
+
+    let mut cmd = get_rgit_cmd();
+    cmd.current_dir(&temp_dir)
+        .arg("add")
+        .arg("a.txt")
+        .assert()
+        .success();
+
+    write(temp_dir.path().join("a.txt"), "modified again").expect("Failed to write to file");
+
+    let mut cmd = get_rgit_cmd();
+    cmd.current_dir(&temp_dir).arg("status").assert().success();
+
+    let expected_output = "Untracked files:
+.rgitignore
+b.txt
+c.txt
+d.txt
+f/g.txt
+k/l/m/o.txt
+k/l/m/q.txt
+l.txt
+run.sh
+Changes to be committed:
+modified: a.txt
+Changed not staged for commit:
+modified: a.txt";
+
+    let output = cmd.output().expect("Failed to run command");
+
+    assert_eq!(
+        String::from_utf8_lossy(&output.stdout).trim(),
+        expected_output
+    );
+
+    Ok(())
+}
